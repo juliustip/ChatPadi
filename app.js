24918d7661ab83b08fee1754e659b32b3a4c8180
AJ <juliustip@gmail.com>
2017-04-17 04:32:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ const express = require("express"),
     app = express(),
     http = require("http").Server(app),
     io = require("socket.io")(http);
-
-http.listen($PORT);
+    var PORT = '3000'
+http.listen(PORT);
 
 app.use(express.static(__dirname + "/public"));
 
